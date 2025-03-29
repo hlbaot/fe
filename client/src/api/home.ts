6efe927d98c2ba23b899e3854f1b4fc55b95e28f
hlbaot <baotoanit1505@gmail.com>
2025-03-30 02:19:01 +0700

    cc

--- client/src/api/home.ts
+++ client/src/api/home.ts
@@ -1,38 +1,3 @@
 const API_Home = import.meta.env.VITE_API_HOME;
 export default API_Home;
 
-
-// [
-//     {
-//       "images": "https://i.pinimg.com/474x/03/8b/dd/038bdd199938e8997091788f05fc7a22.jpg",
-//       "id": "1"
-//     },
-//     {
-//       "images": "https://i.pinimg.com/736x/67/85/c7/6785c7c3f78987b5d9835d67975f03f3.jpg",
-//       "id": "2"
-//     },
-//     {
-//       "images": "https://i.pinimg.com/474x/03/8b/dd/038bdd199938e8997091788f05fc7a22.jpg",
-//       "id": "3"
-//     },
-//     {
-//       "images": "https://i.pinimg.com/236x/ed/fb/0f/edfb0f6332ad1f2e3471620ab6095f57.jpg",
-//       "id": "4"
-//     },
-//     {
-//       "images": "https://i.pinimg.com/736x/67/85/c7/6785c7c3f78987b5d9835d67975f03f3.jpg",
-//       "id": "5"
-//     },
-//     {
-//       "images": "https://i.pinimg.com/236x/ed/fb/0f/edfb0f6332ad1f2e3471620ab6095f57.jpg",
-//       "id": "6"
-//     },
-//     {
-//       "images": "https://i.pinimg.com/736x/67/85/c7/6785c7c3f78987b5d9835d67975f03f3.jpg",
-//       "id": "7"
-//     },
-//     {
-//       "images": "https://i.pinimg.com/236x/ed/fb/0f/edfb0f6332ad1f2e3471620ab6095f57.jpg",
-//       "id": "8"
-//     }
-//   ]
