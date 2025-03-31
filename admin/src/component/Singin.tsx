295fd9636e788442563391c0895f30a000a9b7f3
hlbaot <baotoanit1505@gmail.com>
2025-03-31 10:32:17 +0700

    cc

--- admin/src/component/Singin.tsx
+++ admin/src/component/Singin.tsx
@@ -1,31 +1,47 @@
-import React from 'react'
+import React from 'react';
 import '../assets/styles/Signin.scss';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
 function Singin() {
-  
+ 
+
+  const onSubmit = (values) => {
+    console.log('Dữ liệu form:', values); 
+  };
+
   return (
-    <section className='formSignin'>
+    <section className="formSignin">
       <div className="login-box">
         <p>Login Admin</p>
-        <form>
-          <div className="user-box">
-            <input required  type="text" />
-            <label>Username</label>
-          </div>
-          <div className="user-box">
-            <input required type="password" />
-            <label>Password</label>
-          </div>
-          <a href="#">
-            <span />
-            <span />
-            <span />
-            <span />
-            Submit
-          </a>
-        </form>
+        <Formik
+          initialValues={{ account: '', password: '' }} 
+          onSubmit={onSubmit}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <div className="user-box">
+                <Field type="text" name="account" required />
+                <label>Account</label>
+                <ErrorMessage name="account" component="div" className="error" />
+              </div>
+              <div className="user-box">
+                <Field type="password" name="password" required />
+                <label>Password</label>
+                <ErrorMessage name="password" component="div" className="error" />
+              </div>
+              <button type="submit" className="submit-btn" disabled={isSubmitting}>
+                <span />
+                <span />
+                <span />
+                <span />
+                Submit
+              </button>
+            </Form>
+          )}
+        </Formik>
       </div>
     </section>
-  )
+  );
 }
 
-export default Singin
+export default Singin;
